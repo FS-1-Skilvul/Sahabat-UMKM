7c86c974d0d1d72a7dc871895cc14f8dc6bba9fe
bangkit <bangkitputrakristana@gmail.com>
2023-11-18 20:09:56 +0700

    create kelas model

---
+++ backend/migrations/20231118130507-create-kelas.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Kelas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama_kelas: {
+        type: Sequelize.STRING
+      },
+      deskripsi: {
+        type: Sequelize.TEXT
+      },
+      harga: {
+        type: Sequelize.INTEGER
+      },
+      nama_pengajar: {
+        type: Sequelize.STRING
+      },
+      detail_pengajar: {
+        type: Sequelize.TEXT
+      },
+      durasi: {
+        type: Sequelize.TIME
+      },
+      rating: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Kelas');
+  }
+};
