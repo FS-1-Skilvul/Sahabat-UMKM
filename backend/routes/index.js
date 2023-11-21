7628fd5a561b450f6c194da061fb4e57b4f038d2
bangkit <bangkitputrakristana@gmail.com>
2023-11-21 11:36:26 +0700

    create daftar kelas routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ const route = express.Router();
 
 const kelasRoutes = require("./kelas-routes");
 const kategoriRoutes = require("./kategori-routes");
+const daftarKelasRoutes = require("./daftar-kelas-routes");
 
 route.use("/kelas", kelasRoutes);
 route.use("/kategori", kategoriRoutes);
+route.use("/daftar-kelas", daftarKelasRoutes);
 
 module.exports = route;
