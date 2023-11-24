4b8ad789f41c87fbd8ff22a6492308e4ab8b7300
bangkit <bangkitputrakristana@gmail.com>
2023-11-24 18:51:55 +0700

    complete detail kelas page

--- frontend/src/pages/DetailKelas.jsx
+++ frontend/src/pages/DetailKelas.jsx
@@ -56,30 +56,49 @@ export default function DetailKelas() {
           Apa yang akan dipelajari?
         </h2>
         <div className="p-2 flex gap-3 mb-3 border-2 border-primary rounded-bl-lg rounded-tr-lg w-fit">
-          <span className="w-6 bg-yellow-600 rounded-2xl text-center">1</span>
+          <span className="w-6 bg-yellow-600 rounded-full text-center">1</span>
           <p>Pengenalan Manajemen Keuangan</p>
         </div>
         <div className="p-2 flex gap-3 mb-3 border-2 border-primary rounded-bl-lg rounded-tr-lg w-fit">
-          <span className="w-6 bg-yellow-600 rounded-2xl text-center">2</span>
+          <span className="w-6 bg-yellow-600 rounded-full text-center">2</span>
           <p>Pengenalan Manajemen Keuangan</p>
         </div>
         <div className="p-2 flex gap-3 mb-3 border-2 border-primary rounded-bl-lg rounded-tr-lg w-fit">
-          <span className="w-6 bg-yellow-600 rounded-2xl text-center">3</span>
+          <span className="w-6 bg-yellow-600 rounded-full text-center">3</span>
           <p>Pengenalan Manajemen Keuangan</p>
         </div>
         <div className="p-2 flex gap-3 mb-3 border-2 border-primary rounded-bl-lg rounded-tr-lg w-fit">
-          <span className="w-6 bg-yellow-600 rounded-2xl text-center">4</span>
+          <span className="w-6 bg-yellow-600 rounded-full text-center">4</span>
           <p>Pengenalan Manajemen Keuangan</p>
         </div>
       </section>
-      <section id="detail-mentor" className="px-12 mt-10">
+      <section id="detail-mentor" className="px-12 mt-10 max-w-3xl">
         <h2 className="mb-3 text-[28px] font-semibold text-gray-900">
           Pengajar Kelas
         </h2>
         <div className="border-2 border-primary rounded-bl-lg rounded-tr-lg p-5 w-fit">
+          <div className="flex gap-10 mb-10">
+            <div className="rounded-full overflow-hidden h-14 w-14 border-[1.5px] border-primary">
+              <img
+                src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png"
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div>
+              <h3 className="text-primary text-[20px] font-semibold">
+                Joko Anwar
+              </h3>
+              <span className="text-primary font-semibold">
+                Ahli Manajemen Keuangan dan Konsultan Keuangan
+              </span>
+            </div>
+          </div>
           <p>
-            Pelajari prinsip-prinsip dasar manajemen keuangan. Dari perencanaan
-            anggaran hingga analisis laporan keuangan
+            Joko Anwar adalah seorang profesional berpengalaman dalam bidang
+            manajemen keuangan. Dengan latar belakang yang kaya dalam industri
+            keuangan, ia telah memimpin banyak perusahaan dalam mengelola
+            keuangan mereka dengan efisien.
           </p>
         </div>
       </section>
