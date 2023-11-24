d37ab0b098f7ed8be2f8fa43377efd52f6a1a5bb
bangkit <bangkitputrakristana@gmail.com>
2023-11-24 18:08:49 +0700

    create route and detail kelas page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import Navbar from "./components/navbar";
 import "./App.css";
-import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
-import Hero from "./sections/hero";
-import Kategori from "./sections/Kategori";
-import CardClass from "./components/CardClass";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetailKelas from "./pages/DetailKelas";
+import LandingPage from "./pages/LandingPage";
 import CariKelasPage from "./pages/CariKelasPage";
-import Footer from "./sections/Footer";
-import KelasPopuler from "./sections/kelasPopuler";
 
 function App() {
   return (
     <>
       {/* // <Router> */}
       <Navbar />
-      <Hero />
-
-      <Kategori />
-      <KelasPopuler />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          {/* <Route path="/home" element={<Home />}></Route> */}
+          <Route path="/search" element={<CariKelasPage />}></Route>
+          <Route path="/search/detail-kelas" element={<DetailKelas />}></Route>
+        </Routes>
+      </BrowserRouter>
       {/* <Routes>
         <Route path="/" element={<Beranda />} />
         <Route path="/CariKelasPage" element={<CariKelasPage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,45 +4,46 @@ import { navLinks } from "../constants";
 import { GiHamburgerMenu } from "react-icons/gi";
 import CariKelasPage from "../pages/CariKelasPage";
 // import { Link } from 'react-router-dom';
-function navbar() {
+function Navbar() {
   return (
     <>
-        <header className="sticky top-0  shadow-md">
-          <nav className=" flex justify-between items-center  padding-x py-5 absolute z-10 w-full bg-secondary">
-            <a href="/">
-              <img src={logo} alt="logo" width={140} height={40} />
-            </a>
-            <ul className="flex-1 flex justify-center items-center font-semibold gap-16 max-lg:hidden">
-              {navLinks.map((link) => (
-                <li key={link.label}>
-                  <a href="" className="font-montserrat text-gray-800">
-                {link.label}
-              </a>
-                  {/* <Link to={link.to} className="font-montserrat text-gray-800">
+      <header className="sticky top-0  shadow-md">
+        <nav className=" flex justify-between items-center  padding-x py-5 absolute z-10 w-full bg-secondary">
+          <a href="/">
+            <img src={logo} alt="logo" width={140} height={40} />
+          </a>
+          <ul className="flex-1 flex justify-center items-center font-semibold gap-16 max-lg:hidden">
+            {navLinks.map((link) => (
+              <li key={link.label}>
+                <a href="" className="font-montserrat text-gray-800">
+                  {link.label}
+                </a>
+                {/* <Link to={link.to} className="font-montserrat text-gray-800">
                     {link.label}
                   </Link> */}
-                </li>
-              ))}
-            </ul>
-            <div className="flex gap-2 leading-normal font-montserrat max-lg:hidden wide:mr-24 items-center">
-              <a href="/" className="font px-5 py-1 text-white">
-                Buat Akun
-              </a>
-              {/* <span>/</span> */}
-              <a href="/" className="bg-primary px-8 py-2 rounded-lg font-medium text-white">
-                Masuk
-              </a>
-            </div>
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-2 leading-normal font-montserrat max-lg:hidden wide:mr-24 items-center">
+            <a href="/" className="font px-5 py-1 text-white">
+              Buat Akun
+            </a>
+            {/* <span>/</span> */}
+            <a
+              href="/"
+              className="bg-primary px-8 py-2 rounded-lg font-medium text-white"
+            >
+              Masuk
+            </a>
+          </div>
 
-            <div className="hidden max-lg:block">
-              <GiHamburgerMenu />
-            </div>
-          </nav>
-        </header>
-      
-     
+          <div className="hidden max-lg:block">
+            <GiHamburgerMenu />
+          </div>
+        </nav>
+      </header>
     </>
   );
 }
 
-export default navbar;
+export default Navbar;
