b4a040d5dcc49ee45561cb9167b76b92d7b364ac
bangkitPk <bangkitputrakristana@gmail.com>
2023-11-30 23:38:20 +0700

    add chart to dashboard user use Chart.js

--- frontend/src/pages/DashboardUser.jsx
+++ frontend/src/pages/DashboardUser.jsx
@@ -2,10 +2,32 @@ import { PiHandWavingBold } from "react-icons/pi";
 import { FaRegSave } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
 import { GrCycle } from "react-icons/gr";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function DashboardUser() {
+  // Sample data
+  const data = {
+    labels: ["Sedang Berlangsung", "Diselesaikan"],
+    datasets: [
+      {
+        data: [5, 5],
+        backgroundColor: ["rgba(234, 179, 8, 0.5)", "rgba(34, 197, 94, 0.5)"],
+        borderColor: ["rgba(234, 179, 8, 1)", "rgba(34, 197, 94, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    aspectRatio: 1,
+  };
+
   return (
-    <div className="pt-32 px-12 font-poppins">
+    <div className="pt-12 px-12 font-poppins">
       <h1 className="text-3xl text-primary font-semibold">Dashboard</h1>
       <span className="text-primary text-xl">Halo User </span>
       <PiHandWavingBold className="inline-block text-2xl" />
@@ -32,6 +54,9 @@ export default function DashboardUser() {
           </div>
           <p>Kelas Sedang Berlangsung</p>
         </div>
+      </div>
+      <div className="h-[350px] w-[350px] mt-16">
+        <Pie data={data} options={options} />
       </div>
     </div>
   );
