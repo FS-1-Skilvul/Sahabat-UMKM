55811b6a8071237b7f3385fbf58d9fca9753dfaf
rivazahra <riva.zahra88@gmail.com>
2023-12-06 14:27:20 +0700

    hapus folder be

--- backend/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getUserByToken } = require("../controllers/user.controller");
-
-const route = express.Router();
-
-route.get("/", getUserByToken);
-
-module.exports = route;
